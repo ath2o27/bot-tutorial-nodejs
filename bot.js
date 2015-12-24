215d2efddd5336ec5f010b2c7f4a0df083e17aeb
ath2o27 <jimmy.atwater@gmail.com>
2015-12-23 18:30:14 -0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,27 +1,137 @@
-var HTTPS = require('https');
+
+  var HTTPS = require('https');
 var cool = require('cool-ascii-faces');
 
 var botID = process.env.BOT_ID;
 
 function respond() {
   var request = JSON.parse(this.req.chunks[0]),
-      botRegex = /^\/cool guy$/;
-
+      botRegex = /^\/boot/;  botRegexDL = /^\/dl/i; botRegexSalt = /^\/salt/;botRegexRules = /^\/rules/
+      botRegexAd=/^\/drunk/;botRegexGTA = /^\/yt/; botRegexSC = /^\/sch/i; botODB = /(.*\s+)(.*odb)(\s+.*)/i; botDuck = /^\/duck/;
+      botRegexP = /^\/ply/i;  botRegexTw = /^\/twitch/i; botRegexSb = /^\/red/; botRegexSh = /^\/s/; botRegexWk = /^\/vance/; botRegexCC = /^\/jeff/;
+      botRegexBye = /^\/ptown/; botRegexRip =/^\/richjoseph/; botRegexCan =/^\/mounties/; botRegexTy=/^\/eagles/; botRegexBlog=/^\/blog/; 
+      botRegexL = /^\/l/;
+  var teamAb = ["NE","NO","ARI","PHI","CLE","TEN","OAK","DAL","IND","SEA","CIN","PIT","JAC"
+                ,"BAL","SD","DEN","MIN","ATL","KC","NYG","GB","DET","HOU","STL","CHI","CAR",
+                "MIA","BUF","SF","WAS","NYJ","TB"]
   if(request.text && botRegex.test(request.text)) {
     this.res.writeHead(200);
-    postMessage();
+    postMessage("http://www.landaustore.co.uk/images/footwear/timberland/mens/timberland-mens-timberland-boots-mens-6-inch-boot-brown-37307.jpg");
     this.res.end();
-  } else {
+  } 
+  else if(request.text && botRegexL.test(request.yext)) {
+    this.res.writeHead(200);
+    postMessage("http://imgur.com/mFPQM8B")
+    this.res.end();
+  }
+  else if(request.text && botRegexDL.test(request.text)) {
+    this.res.writeHead(200);
+    postMessage("http://daddyleagues.com/loc/team/"+request.text.substring(4,7)+"/depthchart");
+    this.res.end();
+  }
+  else if(request.text && botRegexTy.test(request.text)) {
+    this.res.writeHead(200);
+    postMessage("http://assets.sbnation.com/assets/562635/eagles_fan.jpg");
+    this.res.end();
+  } 
+  else if(request.text && botRegexSalt.test(request.text)) {
+    this.res.writeHead(200);
+    postMessage("http://i.minus.com/ighljy0ejkExG.gif");
+    this.res.end();
+  } 
+  else if(request.text && botRegexAd.test(request.text)) {
+    this.res.writeHead(200);
+    postMessage("http://i.huffpost.com/gen/1010833/thumbs/o-DESMOND-BRYANT-MUGSHOT-facebook.jpg");
+    this.res.end();
+  }
+  else if(request.text && botRegexRules.test(request.text)) {
+    this.res.writeHead(200);
+    postMessage("http://daddyleagues.com/loc/rules");
+    this.res.end();
+  } 
+  else if(request.text && botRegexGTA.test(request.text)) {
+    this.res.writeHead(200);
+    postMessage("https://www.youtube.com/channel/UC3G0cBakseGjp-mgxfS3bZQ");
+    this.res.end();
+  } 
+  else if(request.text && botRegexSC.test(request.text)) {
+    this.res.writeHead(200);
+    postMessage("http://daddyleagues.com/loc/team/"+request.text.substring(5,8)+"/schedule");
+    this.res.end();
+  }
+  else if(request.text && botRegexP.test(request.text)) {
+    this.res.writeHead(200);
+    var req = request.text.substring(5,request.text.length);
+    var rep = req.replace(/ /,"+");
+    postMessage("http://daddyleagues.com/loc/players?name="+rep+"&position=all&team=all");
+    this.res.end();
+  }  
+  else if(request.text && botRegexTw.test(request.text)) {
+    this.res.writeHead(200);
+    postMessage("http://www.twitch.tv/"+request.text.substring(8,request.text.length));
+    this.res.end();
+  } 
+  else if(request.text && botRegexSb.test(request.text)) {
+    this.res.writeHead(200);
+    postMessage("http://www.reddit.com/r/maddenloc");
+    this.res.end();
+  }  
+  else if(request.text && botRegexRip.test(request.text)) {
+    this.res.writeHead(200);
+    postMessage("http://people-equation.com/wp-content/uploads/RIP_iStock_000013987544XSmall.jpg");
+    this.res.end();
+  }
+  else if(request.text && botRegexBye.test(request.text)) {
+    this.res.writeHead(200);
+    postMessage("http://people-equation.com/wp-content/uploads/RIP_iStock_000013987544XSmall.jpg");
+    this.res.end();
+  }
+  else if(request.text && botRegexSh.test(request.text)) {
+    this.res.writeHead(200);
+    postMessage("¯\\_(ツ)_/¯");
+    this.res.end();
+  } 
+  else if(request.text && botRegexWk.test(request.text)) {
+    this.res.writeHead(200);
+    postMessage("https://cdn0.vox-cdn.com/thumbor/V1yQLz2DcbjHg8vpopX0TDtYDW0=/10x0:4312x2868/730x487/cdn0.vox-cdn.com/uploads/chorus_image/image/44214886/180902191.0.jpg");
+    this.res.end();
+  } 
+  else if(request.text && botODB.test(request.text)) {
+    this.res.writeHead(200);
+    postMessage("OBJ*");
+    this.res.end();
+  } 
+  else if(request.text && botDuck.test(request.text)) {
+    this.res.writeHead(200);
+    postMessage("http://media3.giphy.com/media/YCseTHF2I6CCA/giphy.gif");
+    this.res.end();
+  }
+  else if(request.text && botRegexCC.test(request.text)) {
+    this.res.writeHead(200);
+    postMessage("http://3.bp.blogspot.com/-Hhc6h-HCu8w/UVRXYB6-w8I/AAAAAAAAa0A/LJghsvEd-Pw/s1600/Cheese_205.jpg");
+    this.res.end();
+  }
+  else if(request.text && botRegexCan.test(request.text)) {
+    this.res.writeHead(200);
+    postMessage("http://imagizer.imageshack.us/a/img831/4374/garbagetruckbagtosssave.gif");
+    this.res.end();
+  }
+  else if(request.text && botRegexBlog.test(request.text)) {
+    this.res.writeHead(200);
+    postMessage("http://daddyleagues.com/loc/blog");
+    this.res.end();
+  }
+  else {
     console.log("don't care");
     this.res.writeHead(200);
     this.res.end();
   }
 }
 
-function postMessage() {
-  var botResponse, options, body, botReq;
+function postMessage(response) {
+  var botResponse,options, body, botReq;
 
-  botResponse = cool();
+  botResponse = response
 
   options = {
     hostname: 'api.groupme.com',
@@ -51,6 +161,10 @@ function postMessage() {
     console.log('timeout posting message '  + JSON.stringify(err));
   });
   botReq.end(JSON.stringify(body));
+}
+
+function getRandomInt(min, max) {
+  return Math.floor(Math.random() * (max - min)) + min;
 }
 
 
